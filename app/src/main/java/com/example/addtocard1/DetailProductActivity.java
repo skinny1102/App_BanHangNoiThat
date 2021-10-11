c97a7c7bb7459671c2122eec1ff1e53be879804e
skinny1102 <skinny1102@gmail.com>
2021-10-11 15:23:52 +0700

    lan thu 4

---
+++ app/src/main/java/com/example/addtocard1/DetailProductActivity.java
@@ -0,0 +1,14 @@
+package com.example.addtocard1.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_product);
+    }
+}
